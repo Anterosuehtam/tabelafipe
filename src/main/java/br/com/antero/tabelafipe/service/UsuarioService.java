10c9d3c62b08ec3c8e40fa1d9c1331077d0f7d6c
Matheus Antero <101403401+Anterosuehtam@users.noreply.github.com>
2026-06-01 14:13:13 -0300

    feat: implementa criptografia bcrypt no cadastro de usuarios

--- src/main/java/br/com/antero/tabelafipe/service/UsuarioService.java
+++ src/main/java/br/com/antero/tabelafipe/service/UsuarioService.java
@@ -5,6 +5,7 @@ import br.com.antero.tabelafipe.dto.UsuarioResponseDTO;
 import br.com.antero.tabelafipe.model.Usuario;
 import br.com.antero.tabelafipe.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,12 +14,17 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository repository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public UsuarioResponseDTO cadastrar(UsuarioRequestDTO dados) {
         if (repository.findByEmail(dados.email()).isPresent()) {
             throw new IllegalArgumentException("Esse e-mail já está em uso.");
         }
 
-        Usuario novoUsuario = new Usuario(dados.nome(), dados.email(), dados.senha());
+        String senhaCriptografada = passwordEncoder.encode(dados.senha());
+
+        Usuario novoUsuario = new Usuario(dados.nome(), dados.email(), senhaCriptografada);
 
         Usuario usuarioSalvo = repository.save(novoUsuario);
 
